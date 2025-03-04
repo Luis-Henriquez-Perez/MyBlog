e8a48cb0390b4a8b93ae4fcbc16f317585b408dd
Luis Henriquez-Perez <luis@luishp.xyz>
2025-03-03 21:52:28 -0500

    Update.

--- publish.el
+++ publish.el
@@ -5,26 +5,68 @@
 (setq org-html-htmlize-output-type 'inline-css
       org-html-htmlize-font-prefix "org-")
 
+(defun taingram--sitemap-dated-entry-format (entry style project)
+  "Sitemap PROJECT ENTRY STYLE format that includes date."
+  (cond ((not (directory-name-p entry))
+	     (let* ((file entry)
+		        (title (org-publish-find-title entry project))
+		        (date (format-time-string "%m-%d" (org-publish-find-date entry project)))
+		        (link (concat (file-name-sans-extension entry) ".html")))
+	       (with-temp-buffer
+             (setq file (file-relative-name file default-directory))
+	         (insert (format "[[file:%s][%s ~ %s]]\n" file date title))
+	         (buffer-string))))
+	    ((eq style 'tree)
+	     (file-name-nondirectory (directory-file-name entry)))
+	    (t entry)))
+
+;; Sort sitemap entries by the filename.
+(defun oo--publish-find-date (file _)
+  (string-match "[0-9]\\{4\\}-[0-1][0-9]-[0-3][0-9]T[0-2][0-9].[0-5][0-9].[0-5][0-9]" file)
+  (if-let (timestamp (match-string 0 file))
+      (progn
+        (message "timestamp->%S | file->%S" timestamp file)
+        (encode-time (parse-time-string (string-replace "." ":" timestamp))))
+    (error "No timestamp")))
+
+(advice-add 'org-publish-find-date :override 'oo--publish-find-date)
+
 ;; The reason for keeping using the recursive directory is to preserve the same
 ;; structure.  As in I want the links to work in the org files as well as the
 ;; html files.
 (setq org-publish-project-alist
-      `(("posts"
+      `(("index"
+         :base-directory "org/"
+         :base-extension "org"
+         :exclude ".*"
+         :include ("index.org")
+         :publishing-function org-html-publish-to-html
+         :publishing-directory "html/"
+         :with-title nil
+         :with-creator nil
+         :with-date nil
+         :with-author nil
+         :with-toc nil
+         :section-numbers nil
+         :html-validation-link nil
+         :time-stamp-file nil)
+        ("posts"
          :base-directory "org/"
          :base-extension "org"
          :publishing-directory "html/"
          ;; Ignore files that start with `draft_'.
-         :exclude "draft_.+\\.org$"
+         :exclude "index\\.org$\\|sitemap\\.org$\\|draft_.+\\.org$"
          :publishing-function org-html-publish-to-html
          :recursive t
          :auto-sitemap t
          :sitemap-title "Blog Index"
-         :sitemap-filename "index.org"
+         :sitemap-filename "sitemap.org"
          :sitemap-style list
          ;; I timestamp my files so this should sort them in order by creation date.
-         :sitemap-sort-files chronologically
+         :sitemap-sort-files anti-chronologically
          ;; :sitemap-format-entry "%d %t"
          ;; :sitemap-date-format
+         :sitemap-format-entry taingram--sitemap-dated-entry-format
          :sitemap-file-entry-format "%d %t"
          ;; ----------------------------------- experimental
          :html-doctype "html5"
@@ -57,13 +99,5 @@
         ;;  :recursive t
         ;;  :publishing-function org-publish-attachment)
         ))
-;; Sort sitemap entries by the filename.
-(defun oo--publish-find-date (file _)
-  (string-match "[0-9]\\{4\\}-[0-1][0-9]-[0-3][0-9]T[0-2][0-9].[0-5][0-9].[0-5][0-9]" file)
-  (if-let (timestamp (match-string 0 file))
-      (encode-time (parse-time-string (string-replace "." ":" timestamp)))
-    (error "No timestamp")))
-
-(advice-add 'org-publish-find-date :override 'oo--publish-find-date)
 
 (org-publish-all 'force)
